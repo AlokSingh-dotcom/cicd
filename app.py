9fde6a1300e9082d514ba5b2443278c29b132c75
workacc7717 <workacc7717@gmail.com>
2026-06-25 12:56:57 +0530

    this is for 2nd pract

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+# Define the root route
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+# Run the local development server
+if __name__ == "__main__":
+    app.run(debug=True)
